dd3efe752b39f8fa1adaf266d8fd88d863f614fc
Leroy <sunbutterfly979@gmail.com>
2024-06-24 19:22:51 +0300

    Создала класс Fairy и enum Element

---
+++ Zanzarah_fairy_book/Element.cs
@@ -0,0 +1,17 @@
+namespace Zanzarah_fairy_book;
+
+public enum Element
+{
+    Nature,
+    Stone,
+    Water,
+    Air,
+    Chaos,
+    Psi,
+    Energy,
+    Fire,
+    Ice,
+    Metal,
+    Dark,
+    Light
+}
---
+++ Zanzarah_fairy_book/Fairy.cs
@@ -0,0 +1,16 @@
+namespace Zanzarah_fairy_book;
+
+public class Fairy
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public Element Element { get; set; }
+    public int HitPoints { get; set; }
+    public int Dexterity { get; set; }
+    public int JumpAbility { get; set; }
+    public int Special { get; set; }
+    public bool CanEvolve { get; set; }
+    public int EvolveLevel { get; set; }
+    public Fairy? EvolveForm { get; set; } = null;
+
+}
